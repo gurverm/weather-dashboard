a1300fdb5ea1dfa79fafb8583661003d9e49a35f
Gurveer Madurai <maduraigurveer@gmail.com>
2023-04-10 16:03:23 -0700

    html and js updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,36 @@
 
 // API variables created
-var weatherAPIKey = "19b0fd9b15ce21fd2b0d022f1d7f1a64";
+var weatherAPIKey = "fa91889f3e94337a7424b4068b6b64dc";
 
-var city;
+var city = document.querySelector('#search-form');
 var limit = 5;
 
+var fetchButton = document.getElementById('fetch-button');
 
 // using the geocoding API allows users to time in a city name to fetch results rather than long/lat inputs.
 var geocodingURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=" + limit + "&appid=" + weatherAPIKey;
-fetch(geocodingURL);
 
+var geocodingURL = "api.openweathermap.org/data/2.5/forecast?q="+city+"&appid="{API key}
+// fetch(geocodingURL);
+console.log(geocodingURL);
+
+function getApi() {
+  fetch(geocodingURL)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data)
+    for(var i = 0; i < data.length; i++) {
+      var createTableRow = document.createElement('tr');
+      var tableData = document.createElement('td');
+
+      createTableRow.appendChild(tableData);
+      tableBody.appendChild(createTableRow);
+    }
+  })
+}
+fetchButton.addEventListener('submit', getApi);
 
 
 
